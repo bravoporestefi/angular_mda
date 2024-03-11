72883467c9648272edd23f738ded23666a26a02a
bravoporestefi <bravoporestefi@gmail.com>
2024-03-11 15:13:08 +0100

    Creación del componente home

--- cliente/src/app/app.routes.ts
+++ cliente/src/app/app.routes.ts
@@ -1,11 +1,12 @@
-import { RouterOutlet, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { ClimaComponent } from './pages/clima/clima.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
 
-{path: '', component:RouterOutlet},
-{path: 'clima', component:ClimaComponent},
+    {path: '', component:HomeComponent},
+    {path:'clima', component:ClimaComponent},
 
-{path: '**',redirectTo: '',pathMatch:'full' },
+    {path: '**',redirectTo: '',pathMatch:'full' },
 
 ];
